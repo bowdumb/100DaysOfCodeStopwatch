178874bce59237feb2abbd409f928c8617171e77
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2024-08-03 23:05:24 -0400

    Successfully built clock to counts time on page load.

--- logic.js
+++ logic.js
@@ -1,1 +1,23 @@
-prompt("ALERT!");
+const currentTime = document.getElementById('clock-container');
+
+let secondsCount = 0;
+let minutesCount = 0;
+let hoursCount = 0;
+
+function secondsCounter () {
+    secondsCount++;
+    currentTime.textContent = `${hoursCount}:${minutesCount}:${secondsCount}`;
+
+    if (secondsCount >= 60) {
+        secondsCount = 0;
+        minutesCount++;
+        currentTime.textContent = `${hoursCount}:${minutesCount}:${secondsCount}`;
+    }
+    if (minutesCount >= 60) {
+        minutesCount = 0;
+        hoursCount++;
+        currentTime.textContent = `${hoursCount}:${minutesCount}:${secondsCount}`;
+    }
+}
+
+ setInterval(secondsCounter, 1000);
